a24e4206eaf849af458fd099ee24f755dfe0de3b
Shhatter <bartez378@gmail.com>
2018-04-02 16:45:48 +0200

    0.1 - etap 1

--- Core/EtapPierwszy_MAIN_MAG_WORK.py
+++ Core/EtapPierwszy_MAIN_MAG_WORK.py
@@ -1,4 +1,5 @@
 import glob
+import pathlib
 
 import dlib
 import cv2
@@ -17,13 +18,20 @@ haarFaceCascade = cv2.CascadeClassifier('HaarCascadeConfigs/haarcascade_frontalf
 ###
 
 
-def HaarCascadeFaceDetector(processedImage):
+def HaarCascadeFaceDetector(inputFile):
     dSave = 'Core/Znaleziska algorytmów wyszukiwania/Dlib'
-    grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    grayImage = cv2.cvtColor(inputFile, cv2.COLOR_BGR2GRAY)
     detectedFace = haarFaceCascade.detectMultiScale(grayImage, 1.3, 5)
-    # if detectedFace
-    for (x,y,w,h) in detectedFace:
-        cv2.rectangle(dSave,(x,y),(x+w,y+h),(255,0,0),2)
+    print(detectedFace)
+    print (len(detectedFace))
+
+    # if (detectedFace):
+    # cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    # for (x,y,w,h) in detectedFace:
+    #     cv2.rectangle(dSave,(x,y),(x+w,y+h),(255,0,0),2)
+    cv2.imshow("Image", inputFile)
+    cv2.waitKey(0)
+
 
 def dlibFaceDetector(processedImage):
     hSave = 'Core/Znaleziska algorytmów wyszukiwania/Haar Cascade'
@@ -33,7 +41,16 @@ def lbpCascadeDetector(processedImage):
     lsave = 'Core/Znaleziska algorytmów wyszukiwania/LBP'
 
 
+counter = 0
 for image in lister:
-    HaarCascadeFaceDetector(image)
-    dlibFaceDetector(image)
-    lbpCascadeDetector(image)
+    print(image)
+    HaarCascadeFaceDetector(cv2.imread(image))
+# dlibFaceDetector(image)
+# lbpCascadeDetector(image)
+
+
+# Zmiana nazewnictwa plików bez zmiany rozszerzenia
+# rstrip = pathlib.Path(image).suffix
+# print(rstrip)
+# os.rename(image, "plik " + str(counter)+rstrip)
+# counter += 1
